c327f1532c8ba4d352336c0727785f2d3f075c38
Filipe <filipesilveira.dev@gmail.com>
2025-05-14 13:35:49 -0300

    finalizado (falta revisar o código)

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const limparCampos = () => obterElemento('chute').value='';//estabelece o valor 
 let tentativas = 10; // estabelece a quantidade inicial de tentativas
 let jogoFinalizado = false //variável booleana para indicar o fim do jogo. Seja porque acertou, seja porque acabaram as tentativas
 
-function fazerTentativa(){
+/*function fazerTentativa(){
     if(tentativas<=0 || jogoFinalizado){
         const gg = (`Você foi dibrado(a) pelo bruxo!! O número era o ${numero1e100}. Tente novamente clicando em Recomeçar`)
         dica.innerHTML = ''
@@ -46,7 +46,7 @@ function fazerTentativa(){
     }
 
     return true;//retornar com verdadeiro indica que pode continuar
-}
+}*/
 
 
 
@@ -54,15 +54,25 @@ function chutar(){
     
     console.log(tentativas) 
     
+    //verifica se o jogo acabou antes de iniciar qualquer linha de código
     if(jogoFinalizado){
         alert("O jogo acabou! Clique em Recomeçar para jogar novamente.")
         return
     }
     
-    if(!fazerTentativa()){
+    /*if(!fazerTentativa()){
         return
-    }
+    }*/
 
+    /*if(tentativas===2){
+        const ultimaTentativa = ("Pense bem antes de chutar. É A ÚLTIMA TENTATIVA!!!")
+        dica.innerHTML = ''
+        restent.innerHTML = ultimaTentativa
+        restent.style.textAlign = 'center'
+        restent.style.marginTop = '5px'
+        console.log(ultimaTentativa)
+    }*/
+    
     const valorInformado = obterValorInformado();
     limparCampos()//função criada com o objetivo de limpar os dados inseridos
     chute.focus()//função criada para focar o cursor no elemento com id 'chute', evitando que o usuário precise sempre clicar novamente  
@@ -72,10 +82,53 @@ function chutar(){
             return; // o "return" implica que nenhum passo é dado na função se o número for negativo
         }
 
-    console.log(numero1e100)//pra conferir o número aleatório no console web   
+    //pra conferir o número aleatório no console web
+    console.log(numero1e100)   
 
     //acrescentar for?  
      
+    //se o valor informado for diferente do número aleatório, tentativas decresce 1    
+    if (valorInformado !== numero1e100) {        
+        tentativas--;        
+    }
+
+    //atualiza as tentativas restantes
+    if(tentativas>0){
+        restent.innerHTML =`Tentativas restantes: ${tentativas}`
+    }
+    
+    //se as tetativas forem menor ou igaula à zero, então o jogo acaba
+    if(tentativas<=0){
+        const gg = (`Você foi dibrado(a) pelo bruxo!! O número era o ${numero1e100}. Tente novamente clicando em Recomeçar`)
+        dica.innerHTML = ''
+        restent.innerHTML = gg
+        restent.style.textAlign = 'center'
+        bruxo.src= "/imagens/r10_atento.png"
+        console.log(gg)
+
+        obterElemento('chute').disabled = true
+        obterElemento('btchutar').disabled = true
+
+        jogoFinalizado = true
+        return;
+    }
+
+    //dica se o valor informado for menor e dica se for maior
+    if (valorInformado < numero1e100){
+        const menor = ("Dica: valor informado menor que o escolhido pelo bruxo")
+        dica.innerHTML = menor
+        restent.style.marginBottom = '10px'
+        console.log(menor)
+        //diminuirTentativas()
+
+    } else if (valorInformado > numero1e100){
+        const maior = ("Dica: valor informado maior que o escolhido pelo bruxo")
+        dica.innerHTML = maior
+        restent.style.marginBottom = '10px'
+        console.log(maior)
+        //diminuirTentativas()
+    } 
+    
     if(valorInformado == numero1e100){
         const igual = (`Você descobriu que o segredo do bruxo é ${numero1e100}. Agora você virou o dibrador dos dibradores. Muito bem!!`)
         dica.innerHTML = ''
@@ -85,24 +138,32 @@ function chutar(){
         console.log(igual)
 
         jogoFinalizado=true
-        obterElemento('chute').disabled = true
-        obterElemento('btchutar').disabled = true
+        
+        obterElemento('chute').disabled = true//desabilita o elemento de id 'chute'
+        obterElemento('btchutar').disabled = true//desabilita o elemento de id 'btchutar'
+    }
+    
+    //mensagem exibida alertando para última tentativa
+    if(tentativas ==1){
+        const ultimaTentativa = ("Pense bem antes de chutar. É A ÚLTIMA TENTATIVA!!!")
+        //dica.innerHTML = ''
+            if(valorInformado == numero1e100){//criada para não ficar mostrando 'ultimaTentativa' mesmo após o jogo terminar no console web
+                return ultimaTentativa=''
+            } else{
+                restent.innerHTML = ultimaTentativa
+                restent.style.textAlign = 'center'
+                restent.style.marginTop = '5px'
+                bruxo.src= "/imagens/r10_desafiando.png"
+                //bruxo.style.transition = 'all 10s ease-in-out';
+                //bruxo.style.transform = 'translateX(100px)'; --> desloca o elemento no eixo x
 
-    } else if (valorInformado < numero1e100){
-        const menor = ("Dica: valor informado menor que o escolhido pelo bruxo")
-        dica.innerHTML = menor
-        restent.style.marginBottom = '10px'
-        console.log(menor)
-        diminuirTentativas()
+                console.log(ultimaTentativa)
+            }
+        
+    }
 
-    } else if (valorInformado > numero1e100){
-        const maior = ("Dica: valor informado maior que o escolhido pelo bruxo")
-        dica.innerHTML = maior
-        restent.style.marginBottom = '10px'
-        console.log(maior)
-        diminuirTentativas()
-
-    } else if(tentativas===0 && valorInformado !=numero1e100){
+     
+    /*else if(tentativas===0 && valorInformado !=numero1e100){
         const gg = (`Você foi dibrado(a) pelo bruxo!! O número era o ${numero1e100}. Tente novamente clicando em Recomeçar`)
         dica.innerHTML = ''
         restent.innerHTML = gg
@@ -115,7 +176,7 @@ function chutar(){
         jogoFinalizado = true
         return false;
         console.log("Fim de jogo")
-        }   
+        }   */
     
     
 }
@@ -131,25 +192,7 @@ function chutar(){
 function recomecar(){
     location.reload()
 }
-/*
-function chutar(){
 
-const palpite = document.getElementById("chute").value
-const chute =  10//Number(palpite)
-   
-    if(palpite == numero1e100){
-        console.log("Muito bem!")
-    } else{
-        for(const c = 10; c>=1; c--){
-        console.log(`Tente novamente. O número era ${numero1e100}`)
-        console.log(`Restam ${c} tentativa(s)`)
-        //comparacao()
-        }
-    }
     
     
-}
-*/
-
-
 
